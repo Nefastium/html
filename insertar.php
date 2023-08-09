74245672e24b01ebf545073f330ee74e78ddd5f0
Nefastium <marinkovichezequiel@gmail.com>
2023-08-09 20:44:51 -0300

    a

--- insertar.php
+++ insertar.php
@@ -11,20 +11,20 @@
     <p>hola</p>
     <?php
         echo 'hola' . $nombre;
-    /*$link = mysqli_connect('localhost', 'stefano', 'Elmaspapu1!', 'datos_personales');
+    $link = mysqli_connect('localhost', 'stefano', 'Elmaspapu1!', 'datos_personales');
     if (!$link) {
         die('No se pudo conectar: ' . mysqli_connect_error());
     }
-
+    echo 'hola';
     $nombre = $_POST['nombre'];
     $apellidos = $_POST['apellidos'];
     $direccion = $_POST['direccion'];
     $telefono = $_POST['telefono'];
     $edad = $_POST['edad'];
     $altura = $_POST['altura'];
-    $peso = $_POST['peso'];*/
+    $peso = $_POST['peso'];
 
-    /*$query = "INSERT INTO personas (nombre, apellidos, direccion, telefono, edad, altura, peso)
+    $query = "INSERT INTO personas (nombre, apellidos, direccion, telefono, edad, altura, peso)
     VALUES ('$nombre', '$apellidos', '$direccion', '$telefono', $edad, $altura, $peso)";
 
     if (mysqli_query($link, $query)) {
@@ -32,7 +32,7 @@
     } else {
         echo "Error al insertar el registro: " . mysqli_error($link);
     }
-    mysqli_close($link);*/
+    mysqli_close($link);
     ?>
 
 </body>
