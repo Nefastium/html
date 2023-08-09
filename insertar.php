2ff65c215b808758661f6637432ded383006372f
Nefastium <marinkovichezequiel@gmail.com>
2023-08-09 20:37:04 -0300

    a

--- insertar.php
+++ insertar.php
@@ -4,10 +4,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Mostrar Datos</title>
 </head>
 
 <body>
+    <p>hola</p>
     <?php
     $link = mysqli_connect('localhost', 'stefano', 'Elmaspapu1!', 'datos_personales');
     if (!$link) {
