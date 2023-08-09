90e9eca1dac86b098cade686166ea06a694160c1
Nefastium <marinkovichezequiel@gmail.com>
2023-08-09 18:57:09 -0300

    a

--- ejemplo5.php
+++ ejemplo5.php
@@ -66,7 +66,17 @@ $saludo = "¡Hola!";
 // Vacía el valor de saludo configurándolo como nulo
 $saludo = null;
 ?>
-
-
+    <!--Recursos PHP-->
+<?php
+// imprime: mysql link
+$c = mysql_connect();
+echo get_resource_type($c) . "\n";
+// imprime: stream
+$fp = fopen("foo", "w");
+echo get_resource_type($fp) . "\n";
+// imprime: domxml document
+$doc = new_xmldoc("1.0");
+echo get_resource_type($doc->doc) . "\n";
+?>
 </body>
 </html>
