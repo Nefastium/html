9be28309d0d4a527b65689f97e142b534fa4e1b7
Nefastium <marinkovichezequiel@gmail.com>
2023-08-09 20:38:08 -0300

    a

--- insertar.php
+++ insertar.php
@@ -25,7 +25,7 @@
 
     echo $nombre;
 
-    $query = "INSERT INTO personas (nombre, apellidos, direccion, telefono, edad, altura, peso)
+    /*$query = "INSERT INTO personas (nombre, apellidos, direccion, telefono, edad, altura, peso)
     VALUES ('$nombre', '$apellidos', '$direccion', '$telefono', $edad, $altura, $peso)";
 
     if (mysqli_query($link, $query)) {
@@ -33,8 +33,8 @@
     } else {
         echo "Error al insertar el registro: " . mysqli_error($link);
     }
+    mysqli_close($link);*/
 
-    mysqli_close($link);
     ?>
 
 </body>
